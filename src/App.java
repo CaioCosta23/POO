975e3550308e3fcd2dbf353d40e9959cf4286edf
CaioCosta23 <caio.c.lopes@edu.ufes.br>
2026-01-07 01:01:28 -0300

    Adicionando primeira versão da 'Main' <<Caio>>

--- src/App.java
+++ src/App.java
@@ -1,5 +1,114 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //System.out.println("Hello, World!");
+
+        /*
+         * Linha de Código que limpa o terminal e move o cursor para a primeira linha ao se executar novamente o programa;
+
+         * OBS: Pode não funcionar em alguns consoles ou IDEs mais antigos(as);
+         */
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+
+
+        // Número de linhas que serão ignoradas no arquivo de endereco;
+        final int SALTOS = 2;
+
+
+        Barbearia barbearia = null;
+        Endereco endereco = null;
+
+
+        // Lê o caminho de endereço do arquivo (que neste caso está dentro de uma subpasta);
+        try (InputStream arquivoEndereco = App.class.getResourceAsStream("/dados/endereco.txt")) {
+            // Cria um objeto que permite ler os dados do arquivo da "Stream" criada;
+            BufferedReader br = new BufferedReader(new InputStreamReader(arquivoEndereco));
+            String linha;
+
+            // Lê as linhas do arquivo e as consome no buffer (fazendo com que sejam ignoradas), sem afetar leituras futuras;
+                for (int l = 0; l < SALTOS; l++) {
+                    br.readLine();
+                }
+
+            while ((linha = br.readLine()) != null) {
+                /*
+                 * Lê o a(s) linhas do arquivo (de forma parecida com um arquivo .csv) e divide as informações, usando como referência o ";"
+                 * indicando onde começa e onde termina cada dado/informação;
+                */
+                String[] campos = linha.split(";");
+
+
+                // Trata os erros de conversão de informações para os seus respectivos tipos;
+                try {
+                    // Verifica se é um endereço simples ou um endereço completo e cria o mesmo com as informações retiradas da(s) linha(s) do arquivo;
+                    if (campos.length == 4) {
+                        endereco = new Endereco(Integer.parseInt(campos[0]), campos[1], campos[2], campos[3]);
+                    }else if (campos.length == 7) {
+                        endereco = new Endereco(campos[0], Integer.parseInt(campos[1]), campos[2], campos[3], campos[4], campos[5], campos[6]);
+                    }
+                }catch (NumberFormatException n) {
+                    System.out.println("* Entrada INVALIDA! Insira um dado numerico ao numero do endereco.");
+                }catch (IllegalArgumentException s) {
+                    System.out.println("Entrada INVALIDA! Erro de leitura para os dados descritivos de endereco.");
+                }
+            }
+            
+        } catch (IOException i) {
+            System.out.println("* Erro na leitura/exibicao (do arquivo) de informacoes.");
+        }
+
+
+        try (Scanner entrada = new Scanner(System.in)) {
+            // "Menu" de entrada;
+            System.out.println("===============================================================================================================");
+            System.out.println("\t\t\t\t\tBARBEARIA CIENCIA DA CORTACAO");
+            System.out.println("===============================================================================================================");
+
+            System.out.println("-> Bem-Vindo!\n");
+
+            System.out.println("# Informe o servico do qual deseja acessar:\n");
+
+            System.out.println("\t[1] Acessar area do cliente\t[2] Acessar area do prestador de servico");
+            System.out.println("\t[3] Avaliacoes             \t[4] Informacoes");
+
+            try {
+                System.out.printf("\n- Digite o numero da opcao: ");
+                int opcao = entrada.nextInt();
+                System.out.println("................................................................................................................");
+
+                if ((opcao < 1) || (opcao > 4)) {
+                    throw new IllegalArgumentException();
+                }
+
+                switch(opcao) {
+                    case 1:
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                    case 4:
+                        try {
+                            endereco.imprimeEndereco();
+                        } catch (NullPointerException p) {
+                            System.out.println("* Endereco nao encontrado.");
+                        }
+                        break;
+                    default:
+                        throw new Exception("* Erro de leitura/entrada.");
+                
+                }
+            } catch (IllegalArgumentException o) {
+                System.out.println("* Opcao INVALIDA! Por Favor, digite uma das opcoes listadas acima (1, 2, 3 ou 4).");
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        }
     }
 }
