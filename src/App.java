7b2f4948c2305da70e99a7761e75933520cfec6e
CaioCosta23 <caio.c.lopes@edu.ufes.br>
2026-01-07 13:09:54 -0300

    Inserindo leitura de informações completas da barbearia <<Caio>>

--- src/App.java
+++ src/App.java
@@ -2,12 +2,13 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
+
 
 public class App {
     public static void main(String[] args) throws Exception {
-        //System.out.println("Hello, World!");
-
         /*
          * Linha de Código que limpa o terminal e move o cursor para a primeira linha ao se executar novamente o programa;
 
@@ -17,12 +18,15 @@ public class App {
         System.out.flush();
 
 
+
         // Número de linhas que serão ignoradas no arquivo de endereco;
         final int SALTOS = 2;
 
 
+
         Barbearia barbearia = null;
         Endereco endereco = null;
+
 
 
         // Lê o caminho de endereço do arquivo (que neste caso está dentro de uma subpasta);
@@ -31,11 +35,13 @@ public class App {
             BufferedReader br = new BufferedReader(new InputStreamReader(arquivoEndereco));
             String linha;
 
+
             // Lê as linhas do arquivo e as consome no buffer (fazendo com que sejam ignoradas), sem afetar leituras futuras;
                 for (int l = 0; l < SALTOS; l++) {
                     br.readLine();
                 }
 
+                
             while ((linha = br.readLine()) != null) {
                 /*
                  * Lê o a(s) linhas do arquivo (de forma parecida com um arquivo .csv) e divide as informações, usando como referência o ";"
@@ -52,6 +58,7 @@ public class App {
                     }else if (campos.length == 7) {
                         endereco = new Endereco(campos[0], Integer.parseInt(campos[1]), campos[2], campos[3], campos[4], campos[5], campos[6]);
                     }
+
                 }catch (NumberFormatException n) {
                     System.out.println("* Entrada INVALIDA! Insira um dado numerico ao numero do endereco.");
                 }catch (IllegalArgumentException s) {
@@ -59,55 +66,112 @@ public class App {
                 }
             }
             
+
         } catch (IOException i) {
             System.out.println("* Erro na leitura/exibicao (do arquivo) de informacoes.");
         }
 
 
+
+        // Lê o caminho de endereço do arquivo (que neste caso está dentro de uma subpasta);
+        try (InputStream arquivoInformacoes = App.class.getResourceAsStream("/dados/infoBarbearia.txt")) {
+            // Cria um objeto que permite ler os dados do arquivo da "Stream" criada;
+            BufferedReader info = new BufferedReader(new InputStreamReader(arquivoInformacoes));
+            String dados;
+
+
+            // Lê as linhas do arquivo e as consome no buffer (fazendo com que sejam ignoradas), sem afetar leituras futuras;
+                for (int l = 0; l < SALTOS; l++) {
+                    info.readLine();
+                }
+
+
+            while ((dados = info.readLine()) != null) {
+                /*
+                 * Lê o a(s) linhas do arquivo (de forma parecida com um arquivo .csv) e divide as informações, usando como referência o ";"
+                 * indicando onde começa e onde termina cada dado/informação;
+                */
+                String[] informacoes = dados.split(";");
+
+
+                // Trata os erros de conversão de informações para os seus respectivos tipos;
+                try {
+                    LocalTime abertura = LocalTime.parse(informacoes[3], DateTimeFormatter.ofPattern("HH:mm"));
+                    LocalTime fechamento = LocalTime.parse(informacoes[4], DateTimeFormatter.ofPattern("HH:mm"));
+
+                    barbearia = new Barbearia(informacoes[0], endereco, informacoes[1], informacoes[2], abertura, fechamento, informacoes[5]);
+
+
+                }catch (NumberFormatException n) {
+                    System.out.println("* Entrada INVALIDA! Insira um dado numerico ao numero do endereco.");
+                }catch (IllegalArgumentException h) {
+                    System.out.println("Entrada INVALIDA! Erro de leitura para os dados de horarios.");
+                }
+            }
+
+            
+        } catch (IOException i) {
+            System.out.println("* Erro na leitura/exibicao (do arquivo) de informacoes.");
+        }
+
+
+
         try (Scanner entrada = new Scanner(System.in)) {
-            // "Menu" de entrada;
-            System.out.println("===============================================================================================================");
-            System.out.println("\t\t\t\t\tBARBEARIA CIENCIA DA CORTACAO");
-            System.out.println("===============================================================================================================");
-
-            System.out.println("-> Bem-Vindo!\n");
-
-            System.out.println("# Informe o servico do qual deseja acessar:\n");
-
-            System.out.println("\t[1] Acessar area do cliente\t[2] Acessar area do prestador de servico");
-            System.out.println("\t[3] Avaliacoes             \t[4] Informacoes");
-
             try {
-                System.out.printf("\n- Digite o numero da opcao: ");
-                int opcao = entrada.nextInt();
-                System.out.println("................................................................................................................");
-
-                if ((opcao < 1) || (opcao > 4)) {
-                    throw new IllegalArgumentException();
+                // "Menu" de entrada;
+                if (barbearia == null) {
+                    throw new NullPointerException();
                 }
 
-                switch(opcao) {
-                    case 1:
-                        break;
-                    case 2:
-                        break;
-                    case 3:
-                        break;
-                    case 4:
-                        try {
-                            endereco.imprimeEndereco();
-                        } catch (NullPointerException p) {
-                            System.out.println("* Endereco nao encontrado.");
-                        }
-                        break;
-                    default:
-                        throw new Exception("* Erro de leitura/entrada.");
+
+                System.out.println("===============================================================================================================");
+                System.out.println("\t\t\t\t\t" + barbearia.getNome());
+                System.out.println("===============================================================================================================");
+
+                System.out.println("-> Bem-Vindo!\n");
+
+                System.out.println("# Informe o servico do qual deseja acessar:\n");
+
+                System.out.println("\t[1] Acessar area do cliente\t[2] Acessar area do prestador de servico");
+                System.out.println("\t[3] Avaliacoes             \t[4] Informacoes");
+
+
+                try {
+                    System.out.printf("\n- Digite o numero da opcao: ");
+                    int opcao = entrada.nextInt();
+                    System.out.println("................................................................................................................");
+
+                    if ((opcao < 1) || (opcao > 4)) {
+                        throw new IllegalArgumentException();
+                    }
+
+                    switch(opcao) {
+                        case 1:
+                            break;
+                        case 2:
+                            break;
+                        case 3:
+                            break;
+                        case 4:
+                            try {
+                                barbearia.exibirInformacoes();
+                            } catch (NullPointerException p) {
+                                System.out.println("* Informacoes nao encontradas.");
+                            }
+                            break;
+                        default:
+                            throw new Exception("* Erro de leitura/entrada.");
+                    
+                    }
+                } catch (IllegalArgumentException o) {
+                    System.out.println("* Opcao INVALIDA! Por Favor, digite uma das opcoes listadas acima (1, 2, 3 ou 4).");
+                } catch (Exception e) {
+                    System.out.println(e.getMessage());
+                }
                 
-                }
-            } catch (IllegalArgumentException o) {
-                System.out.println("* Opcao INVALIDA! Por Favor, digite uma das opcoes listadas acima (1, 2, 3 ou 4).");
-            } catch (Exception e) {
-                System.out.println(e.getMessage());
+
+            } catch (NullPointerException i) {
+                System.out.println("* Informacoes nao encontradas.");
             }
         }
     }
