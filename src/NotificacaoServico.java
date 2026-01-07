a092b8e12392ded5f9455ca7bb260fa8d5b3d34b
CaioCosta23 <caio.c.lopes@edu.ufes.br>
2026-01-06 21:56:56 -0300

    Código inicial de Notificacao de servico <<Caio>>

--- src/NotificacaoServico.java
+++ src/NotificacaoServico.java
@@ -1,3 +1,37 @@
 public class NotificacaoServico {
+    private int id;
+    private EnumTipoNotificacao tipo;
     
+    public NotificacaoServico(int id, EnumTipoNotificacao tipo) {
+        this.id = id;
+        this.tipo = tipo;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public EnumTipoNotificacao getTipo() {
+        return this.tipo;
+    }
+
+    public void setTipo(EnumTipoNotificacao tipo) {
+        this.tipo = tipo;
+    }
+
+    public void enviarEmail() {
+        System.out.println();
+    }
+
+    public void enviarSMS() {
+        System.out.println();
+    }
+
+    public void enviarPush() {
+        System.out.println();
+    }
 }
