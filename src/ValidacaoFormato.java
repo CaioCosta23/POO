cd4206479dcec3bd2fc7ca60723e9c1157e180c1
CaioCosta23 <caio.c.lopes@edu.ufes.br>
2026-01-09 10:05:44 -0300

    Adicionando arquivo de validação de informaçõe gerais (CEP, CPF... etc) <<Caio>>

---
+++ src/ValidacaoFormato.java
@@ -0,0 +1,37 @@
+public class ValidacaoFormato {
+    /*
+     * EXPRESSÃO REGULAR:
+     *
+     * Trata-se de uma expressão que define um padrão de escrita/validação de algum dado (geralmente também descritivo);
+     * Não depende do objeto, sendo uma regra/expressão fixa, fazendo com que o proograma seja mais eficiente e sem a necessidade de instanciação de um objeto;
+     * Não pode ser alterada, evitando bugs acidentais e garantindo consistência para o código;
+     * 
+     * Destrinchando a expressão abaixo (em si):
+     * 
+     * O "^" indica que não pode haver mais nada antes disso;
+     * "0-9" representa a possibilidade de haver todo e qualquer algarismo de 0 à 9;
+     * "$" indica que não pode haver mais nenhum caractere após isso;
+     * 
+     * OBS: Geralmente essa variável é nomeada como "REGEX";
+     */
+
+
+    /*
+     * Este trecho de código realiza a validação de um CEP, verificando se o mesmo está no formato padrão utilizado ou não;
+     */
+    public static void validacao(String dado, String formato) throws ExceptionFormato {
+        // Verifica se o CPF foi preenchido na hora da captura dos dados ou se o campo está vazio;
+        if ((dado == null) || dado.isBlank()) {
+            throw new ExceptionFormato("CEP vazio ou inexistente.");
+        }
+
+        /*
+         * Verifica se o CEP está no formato estipulado;
+         * .matches(): verifica se a string (neste caso) segue 100% a regra estipulada no REGEXCEP; 
+        */
+
+        if (!(dado).matches(formato)) {
+            throw new ExceptionFormato("CEP Invalido.");
+        }
+    }
+}
