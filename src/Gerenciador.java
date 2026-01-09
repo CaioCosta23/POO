0ee67e4657f3dec57ed8cffbc5c959404a638853
CaioCosta23 <caio.c.lopes@edu.ufes.br>
2026-01-09 12:39:10 -0300

    Inserindo novo formato de tratamento de exceção <<Caio>>

--- src/Gerenciador.java
+++ src/Gerenciador.java
@@ -17,45 +17,39 @@ public class Gerenciador {
     }
 
 
-    public void executarPrograma(Barbearia barbearia, Set <Cliente> clientes, Leitor leitor) {
+    public void executarPrograma(Barbearia barbearia, Set <Cliente> clientes, Leitor leitor) throws Exception {
         final int QTDOPCOES = 3;
 
-
         int opcao = leitor.leOpcoes(QTDOPCOES);
-        try {
-            if ((opcao < 1) || (opcao > QTDOPCOES)) {
-                throw new Exception("* Erro de leitura/entrada.");
-            }
-            if ((opcao == 1) || (opcao == 2)) {
-                // Acesso à area do Pretador de Serviços (Barbeiro ou Administrador - do Sistema);
-                System.out.print("\033[H\033[2J");
-                System.out.flush();  
 
-                if (opcao == 1) {
-                    Cliente cliente = (Cliente)leitor.lerLoginSenhaUsuarios(clientes);
-                    if (cliente == null) {
-                        throw new ExceptionObjetoInexistente("Usuario ou objeto nao encontrado.");
-                    }
-                }
-                if (opcao == 2) {
-                    Barbeiro barbeiro = (Barbeiro)leitor.lerLoginSenhaUsuarios(barbearia.getBarbeiros());
-                    if (barbeiro == null) {
-                        throw new ExceptionObjetoInexistente("Usuario ou objeto nao encontrado.");
-                    }
-                }
-            }
-            if (opcao == 3) {
-                // Imprime as informações da barearia (se as mesmas existirem!);
-                try {
-                    barbearia.exibirInformacoes();
-                } catch (NullPointerException p) {
-                    System.out.println("* Informacoes nao encontradas.");
+        if ((opcao < 1) || (opcao > QTDOPCOES)) {
+            throw new IllegalArgumentException("* Erro de leitura/entrada.");
+        }
+        if ((opcao == 1) || (opcao == 2)) {
+            // Acesso à area do Pretador de Serviços (Barbeiro ou Administrador - do Sistema);
+            System.out.print("\033[H\033[2J");
+            System.out.flush();  
+
+            if (opcao == 1) {
+                Cliente cliente = (Cliente)leitor.lerLoginSenhaUsuarios(clientes);
+                if (cliente == null) {
+                    throw new ExceptionObjetoInexistente("Usuario ou objeto nao encontrado.");
                 }
             }
-        }catch (ExceptionObjetoInexistente u) {
-            System.out.println(u.getMessage());
-        } catch (Exception o) {
-            System.out.println(o.getMessage());
+            if (opcao == 2) {
+                Barbeiro barbeiro = (Barbeiro)leitor.lerLoginSenhaUsuarios(barbearia.getBarbeiros());
+                if (barbeiro == null) {
+                    throw new ExceptionObjetoInexistente("Usuario ou objeto nao encontrado.");
+                }
+            }
+        }
+        if (opcao == 3) {
+            // Imprime as informações da barearia (se as mesmas existirem!);
+            try {
+                barbearia.exibirInformacoes();
+            } catch (NullPointerException p) {
+                System.out.println("* Informacoes nao encontradas.");
+            }
         }
     }
 }
