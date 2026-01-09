be2a7f95ced6128ec74ba4268008673281726c56
CaioCosta23 <caio.c.lopes@edu.ufes.br>
2026-01-09 11:23:25 -0300

    Iserindo temporizador de instruções <<Caio>>

--- src/App.java
+++ src/App.java
@@ -1,5 +1,6 @@
 import java.util.HashSet;
 import java.util.Set;
+import java.util.concurrent.TimeUnit;
 
 public class App {
     public static void main(String[] args) throws Exception {
@@ -10,6 +11,8 @@ public class App {
          */
         System.out.print("\033[H\033[2J");
         System.out.flush();
+
+        TimeUnit.SECONDS.sleep(2);
         
 
         Barbearia barbearia = null;
@@ -18,9 +21,15 @@ public class App {
         Leitor leitor = new Leitor();
         Gerenciador gerenciador = new Gerenciador();
 
-
+        
         barbearia = leitor.leDadosBarbearia(barbearia);
         leitor.leEndereco(barbearia);
+
+        // Cria um tempo de espera (em segundos) no terminal antes de executar as próximas açõe;
+        TimeUnit.SECONDS.sleep(6);
+
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
 
         gerenciador.iniciarSistema(barbearia);
         gerenciador.executarPrograma(barbearia, clientes, leitor);
