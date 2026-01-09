74c0cda84050580cb61e8f58869b5a911ab852df
CaioCosta23 <caio.c.lopes@edu.ufes.br>
2026-01-09 12:30:27 -0300

    Inserindo tratamento especial para a falta de endereço conclusivo <<Caio>>

--- src/Leitor.java
+++ src/Leitor.java
@@ -8,66 +8,10 @@ import java.util.Iterator;
 import java.util.Map;
 import java.util.Scanner;
 import java.util.Set;
+import java.util.concurrent.TimeUnit;
 
 public class Leitor {
     public Leitor() {
-    }
-
-    public void leEndereco(Barbearia barbearia) {
-        final int SALTOS = 2;
-
-         Endereco endereco = null;
-
-        // Lê o caminho de endereço do arquivo (que neste caso está dentro de uma subpasta);
-        try (InputStream arquivoEndereco = App.class.getResourceAsStream("/dados/endereco.txt")) {
-            // Cria um objeto que permite ler os dados do arquivo da "Stream" criada;
-            BufferedReader br = new BufferedReader(new InputStreamReader(arquivoEndereco));
-            String linha;
-
-
-            // Lê as linhas do arquivo e as consome no buffer (fazendo com que sejam ignoradas), sem afetar leituras futuras;
-                for (int l = 0; l < SALTOS; l++) {
-                    br.readLine();
-                }
-
-                
-            while ((linha = br.readLine()) != null) {
-                /*
-                 * Lê o a(s) linhas do arquivo (de forma parecida com um arquivo .csv) e divide as informações, usando como referência o ";"
-                 * indicando onde começa e onde termina cada dado/informação;
-                */
-                String[] campos = linha.split(";");
-
-
-                // Trata os erros de conversão de informações para os seus respectivos tipos;
-                try {
-
-                    // Verifica se é um endereço simples ou um endereço completo e cria o mesmo com as informações retiradas da(s) linha(s) do arquivo;
-                    if (campos.length == 4) {
-                        ValidacaoCep.validacao(campos[3]);
-                        endereco = new Endereco(Integer.parseInt(campos[0]), campos[1], campos[2], campos[3]);
-                    }else if (campos.length == 7) {
-                        ValidacaoCep.validacao(campos[6]);
-                        endereco = new Endereco(campos[0], Integer.parseInt(campos[1]), campos[2], campos[3], campos[4], campos[5], campos[6]);
-                    }
-
-                    barbearia.setEndereco(endereco);
-
-
-                }catch (NumberFormatException n) {
-                    System.out.println("* Entrada INVALIDA! Insira um dado numerico ao numero do endereco.");
-                }catch (IllegalArgumentException s) {
-                    System.out.println("Entrada INVALIDA! Erro de leitura para os dados descritivos de endereco.");
-                }catch (ExceptionFormato c) {
-                    System.out.println("* Entrada de dados INVALIDA! " + c.getMessage());
-                }
-            }
-            
-
-
-        } catch (IOException i) {
-            System.out.println("* Erro na leitura/exibicao (do arquivo) de informacoes.");
-        }
     }
 
     public Barbearia leDadosBarbearia(Barbearia barbearia) {
@@ -78,14 +22,13 @@ public class Leitor {
         try (InputStream arquivoInformacoes = App.class.getResourceAsStream("/dados/infoBarbearia.txt")) {
             // Cria um objeto que permite ler os dados do arquivo da "Stream" criada;
             BufferedReader info = new BufferedReader(new InputStreamReader(arquivoInformacoes));
+
             String dados;
 
-
             // Lê as linhas do arquivo e as consome no buffer (fazendo com que sejam ignoradas), sem afetar leituras futuras;
-                for (int l = 0; l < SALTOS; l++) {
-                    info.readLine();
-                }
-
+            for (int l = 0; l < SALTOS; l++) {
+                info.readLine();
+            }
 
             while ((dados = info.readLine()) != null) {
                 /*
@@ -94,39 +37,89 @@ public class Leitor {
                 */
                 String[] informacoes = dados.split(";");
 
-
-                // Trata os erros de conversão de informações para os seus respectivos tipos;
+                // Trata os erros de conversão e erros de leituras de informações e para os seus respectivos tipos;
                 try {
-                    // Faz a validação dos respectivos dados e verifica se os mesmos estão no formato correto;
-                    ValidacaoEmail.validacao(informacoes[2]);
-                    ValidacaoHorario.validacao(informacoes[3]);
-                    ValidacaoHorario.validacao(informacoes[4]);
-                    ValidacaoCnpj.validacao(informacoes[5]);
+                    ValidacaoFormato.validacao(informacoes[2], EnumFormato.EMAIL.FORMATO);
+                    ValidacaoFormato.validacao(informacoes[3], EnumFormato.HORARIO.FORMATO);
+                    ValidacaoFormato.validacao(informacoes[4], EnumFormato.HORARIO.FORMATO);
+                    ValidacaoFormato.validacao(informacoes[5], EnumFormato.CNPJ.FORMATO);
 
-                    
+                    // Coverte a informação de horário lida de "String" para "LocalTime";
                     LocalTime abertura = LocalTime.parse(informacoes[3], DateTimeFormatter.ofPattern("HH:mm"));
                     LocalTime fechamento = LocalTime.parse(informacoes[4], DateTimeFormatter.ofPattern("HH:mm"));
                     
-
+                    // Cria a barbearia com as informações lidas no arquivo (e o endereço será lido em outro método - aqui ele apenas é "inicializado");
                     barbearia = new Barbearia(informacoes[0], null, informacoes[1], informacoes[2], abertura, fechamento, informacoes[5]);
-
-
                 }catch (ExceptionFormato em) {
-                    System.out.println("* Entrada de dados INVALIDA! " + em.getMessage());
+                    System.out.println(em.getMessage());
                 }catch (IllegalArgumentException h) {
                     System.out.println("Entrada INVALIDA! Erro de leitura para os dados de horarios.");
                 }catch (NullPointerException b) {
                     System.out.println("Erro! Dados do sistema da barbearia nao encontrados.");
                 }
             }
-
-            
+        // Trata os erros de leitura do arquivo;
         } catch (IOException i) {
             System.out.println("* Erro na leitura/exibicao (do arquivo) de informacoes.");
         }
-        
         return barbearia;
     }
+
+
+    public void leEndereco(Barbearia barbearia) {
+        final int SALTOS = 2;
+
+        try (InputStream arquivoEndereco = App.class.getResourceAsStream("/dados/endereco.txt")) {
+            BufferedReader br = new BufferedReader(new InputStreamReader(arquivoEndereco));
+
+            String linha;
+
+            for (int l = 0; l < SALTOS; l++) {
+                br.readLine();
+            }
+                
+            while ((linha = br.readLine()) != null) {
+                String[] campos = linha.split(";");
+                
+                try {
+                    /*
+                     * Verifica se é um endereço simples ou um endereço completo e cria o mesmo com as informações retiradas da(s) linha(s) do arquivo;
+
+                     *OBS: Verifique o arquivo de entrada "endereco.txt" para entender melhor o formato dos dados;
+                    */
+                    if (campos.length == 4) {
+                        ValidacaoFormato.validacao(campos[3], EnumFormato.CEP.FORMATO);
+                        barbearia.setEndereco(new Endereco(Integer.parseInt(campos[0]), campos[1], campos[2], campos[3]));
+                    }else if (campos.length == 7) {
+                        ValidacaoFormato.validacao(campos[6], EnumFormato.CEP.FORMATO);
+                        barbearia.setEndereco(new Endereco(campos[0], Integer.parseInt(campos[1]), campos[2], campos[3], campos[4], campos[5], campos[6]));
+                    }
+                }catch (NumberFormatException n) {
+                    System.out.println("* Entrada INVALIDA! Insira um dado numerico ao numero do endereco.");
+                }catch (IllegalArgumentException s) {
+                    System.out.println("Entrada INVALIDA! Erro de leitura para os dados descritivos de endereco.");
+                }catch (ExceptionFormato c) {
+                    System.out.println("|AVISO|");
+                    System.out.println(c.getMessage() + " Erro na validacao do formato de CEP.");
+                    System.out.println("* O endereco criado com campos nulos/incompletos e sem informacoes.");
+                    barbearia.setEndereco(new Endereco(-1 , "-", "-", "-"));
+                    
+                    try {
+                        // Cria um tempo de espera (em segundos) no terminal antes de executar as próximas açõe;
+                        TimeUnit.SECONDS.sleep(6);
+
+                        System.out.print("\033[H\033[2J");
+                        System.out.flush();
+                    } catch (InterruptedException e) {
+                        Thread.currentThread().interrupt();
+                    }
+                }
+            }
+        } catch (IOException i) {
+            System.out.println("* Erro na leitura/exibicao (do arquivo) de informacoes.");
+        }
+    }
+
 
     public int leOpcoes(int qtdOpcoes) {
         int opcao = -1;
