809b4dfead6a782c81ee980bfe8c4ee4a794c3f1
CaioCosta23 <caio.c.lopes@edu.ufes.br>
2026-01-08 23:29:11 -0300

    Reduzindo ao minímo o arquivo main <<Caio>>

--- src/App.java
+++ src/App.java
@@ -12,60 +12,20 @@ public class App {
          */
         System.out.print("\033[H\033[2J");
         System.out.flush();
-
-
-        final String CODIGO = "15.457.146/7923-48";
-        final int QTDOPCOES = 3;
-
+        
 
         Map <String, Barbearia> barbearias = new HashMap<>();
         Set <Cliente> clientes = new HashSet<>();
-        Map <String, Barbeiro> barbeiros = new HashMap<>();
 
         Leitor leitor = new Leitor();
+        Gerenciador gerenciador = new Gerenciador();
+
+
         leitor.leDadosBarbearia(barbearias);
-        leitor.leEndereco(barbearias, CODIGO);
+        leitor.leEndereco(barbearias, gerenciador.getCodigo());
 
-        // Verifica se a barbearia existe dentro do sistema;
-        try {
-            if (barbearias.isEmpty()) {
-                throw new Exception("* Barbearia(s) inexistente(s) no sistema. ");
-            }
+        gerenciador.iniciarSistema(barbearias);
+        gerenciador.executarPrograma(barbearias, clientes, leitor);
 
-            barbearias.get(CODIGO).exibirMenu();
-
-        }catch (Exception v) {
-            System.out.println(v.getMessage() + "Nao existem informacoes no sistema.");
-        }
-
-        
-        int opcao = leitor.leOpcoes(QTDOPCOES);
-        try {
-            if ((opcao < 1) || (opcao > QTDOPCOES)) {
-                throw new Exception("* Erro de leitura/entrada.");
-            }
-            if ((opcao == 1) || (opcao == 2)) {
-                // Acesso à area do Pretador de Serviços (Barbeiro ou Administrador - do Sistema);
-                System.out.print("\033[H\033[2J");
-                System.out.flush();
-
-                if (opcao == 1) {
-                    
-                }
-                if (opcao == 2) {
-
-                }
-            }
-            if (opcao == 3) {
-                // Imprime as informações da barearia (se as mesmas existirem!);
-                try {
-                    barbearias.get(CODIGO).exibirInformacoes();
-                } catch (NullPointerException p) {
-                    System.out.println("* Informacoes nao encontradas.");
-                }
-            }
-        } catch (Exception o) {
-            System.out.println(o.getMessage());
-        }
     }
 }
