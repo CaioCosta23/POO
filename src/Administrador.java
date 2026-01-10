f21dfb427f57f567bf2449c509d142c47dcdc4b4
CaioCosta23 <caio.c.lopes@edu.ufes.br>
2026-01-09 21:05:23 -0300

    Adicionando código inicial de administardor

--- src/Administrador.java
+++ src/Administrador.java
@@ -1,3 +1,26 @@
 public class Administrador extends Usuario {
+
+    public Administrador(int id, String nome, String email, String telefone, String cpf, String login, String senha) {
+        super(id, nome, email, telefone, cpf, login, senha);
+    }
+
+    @Override
+    public void exibirMenu() {
+        System.out.println("@ Selecione uma das opcoes abaixo:\n");
+
+        System.out.println("[1]  Criar agendamento\t[2] Excluir Agendamento");
+        System.out.println("[4] Adicionar Servico\t[4] Remover Servico");
+        System.out.println("[5] Adicioanar Usuario\t[] Excluir Usuario");
+        System.out.println("[7] Consultar agendamento\t[8] Consultar Dados Usuarios ");
+        System.out.println("[9] Consultar Servico\t[10] Consultar Dados Pessoais ");
+    }
+
+
+    @Override
+    public void exibirInformacoes() {
+        System.out.println("- Nome: "+ this.getNome());
+        System.out.println("- E-mail: " + this.getEmail());
+        System.out.println("- Telefone: " + this.getTelefone());
+    }
     
 }
