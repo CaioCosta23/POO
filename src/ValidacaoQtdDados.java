398e2d9c3e4481f5f1943361a7640e57ca0c0f4d
CaioCosta23 <caio.c.lopes@edu.ufes.br>
2026-01-11 14:53:14 -0300

    Alterando parâmetros de função de validação

--- src/ValidacaoQtdDados.java
+++ src/ValidacaoQtdDados.java
@@ -1,6 +1,5 @@
 public class ValidacaoQtdDados {
-    public static final void validacao(String linha, String nomeArquivo, int qtdDados) throws IllegalArgumentException{
-        String[]campos = linha.split(";");
+    public static final void validacao(String[] campos, String nomeArquivo, int qtdDados) throws IllegalArgumentException{
 
         if (campos.length != qtdDados) {
             throw new IllegalArgumentException("com quantidade incorreta de dados do arquivo " + nomeArquivo);
