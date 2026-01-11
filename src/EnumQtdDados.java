9f90e38c26d1539267879a4dbbe4360f1b008bb2
CaioCosta23 <caio.c.lopes@edu.ufes.br>
2026-01-11 14:11:28 -0300

    Adicinando Enumeration para quantidade de dados em uma linha de arquivo

---
+++ src/EnumQtdDados.java
@@ -0,0 +1,17 @@
+public enum EnumQtdDados {
+    QTD_DADOS_CLIENTES(8),
+    QTD_DADOS_BARBEIRO(7),
+    QTD_DADOS_SERVICOS(6),
+    QTD_DADOS_ENDERECO_SIMPLES(5),
+    QTD_DADOS_EMDERECO_COMPLETO(8);
+
+    private final int quantidade;
+
+    EnumQtdDados(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public int getValue() {
+        return this.quantidade;
+    }
+}
