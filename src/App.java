8279b364be8fb5a3cceef6e2004182e7584834be
CaioCosta23 <caio.c.lopes@edu.ufes.br>
2026-01-11 11:48:59 -0300

    Inserindo todos os tratamentos de erros

--- src/App.java
+++ src/App.java
@@ -1,4 +1,5 @@
 import java.io.FileNotFoundException;
+import java.util.NoSuchElementException;
 import java.util.concurrent.TimeUnit;
 
 public class App {
@@ -11,12 +12,13 @@ public class App {
         System.out.print("\033[H\033[2J");
         System.out.flush();
 
+        // Cria um tempo de espera na saída;
         TimeUnit.SECONDS.sleep(2);
         
 
         Leitor leitor = new Leitor();
         Gerenciador gerenciador = new Gerenciador();
-
+        /* */
         try {
             Barbearia barbearia = leitor.leDadosBarbearia(EnumCaminho.BARBEARIA.getValue());
 
@@ -25,11 +27,30 @@ public class App {
         } catch (FileNotFoundException a) {
             System.out.println("* Erro na leitura do arquivo! Um dos arquivos de confinguracao e leitura nao foi encontrado.");
         } catch (NullPointerException v) {
-            System.out.println("Erro! Dado(s) nao encontrado(s) ou nao lidos.");
+            System.out.format("Erro! Dado(s) de", v.getMessage(), " nao encontrado(s) ou nao lidos ou incompletos.\n");
         }catch (ExceptionObjetoInexistente o) {
             System.out.println("Erro no registro/obtencao de dados!" + o.getMessage());
-        } catch (IllegalArgumentException s) {
-            System.out.println("* Erro de leitura para os dados descritivos da barbearia.");
+        }catch(ExceptionFormato f) {
+            System.out.format("Erro! Formato do(s) ", f.getMessage(), " incompativel para a leitura/armazenamento das informacoes.");
+        } catch (IllegalArgumentException a) {
+            System.out.println("* Erro de leitura dos dados " + a.getMessage());
+        } catch(NoSuchElementException q) {
+            System.out.println("\n................................................................................................................");
+            System.out.println("$ Programa encerrado prematuramente.\n");
+            // Faz com que o programa não execute o finally por lançar uma InterruptedException;
+            TimeUnit.SECONDS.sleep(1);
+        } finally {
+            System.out.println("................................................................................................................");
+            System.out.printf("Finalizando");
+            TimeUnit.SECONDS.sleep(1);
+            System.out.printf(".");
+            TimeUnit.SECONDS.sleep(1);
+            System.out.printf(".");
+            TimeUnit.SECONDS.sleep(1);
+            System.out.printf(".\n");
+            TimeUnit.SECONDS.sleep(1);
+            System.out.println("................................................................................................................");
+            System.out.println("+ ENCERRADO.");
         }
     }
 }
