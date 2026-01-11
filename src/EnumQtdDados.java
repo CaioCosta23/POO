bfb72048ff5809ee48ff4be61ae18ef2802ea19f
CaioCosta23 <caio.c.lopes@edu.ufes.br>
2026-01-11 14:26:10 -0300

    Adicionando quantidade para linha de dados da barbearia

--- src/EnumQtdDados.java
+++ src/EnumQtdDados.java
@@ -3,7 +3,8 @@ public enum EnumQtdDados {
     QTD_DADOS_BARBEIRO(7),
     QTD_DADOS_SERVICOS(6),
     QTD_DADOS_ENDERECO_SIMPLES(5),
-    QTD_DADOS_EMDERECO_COMPLETO(8);
+    QTD_DADOS_EMDERECO_COMPLETO(8),
+    QTD_DADOS_BARBEARIA(6);
 
     private final int quantidade;
 
