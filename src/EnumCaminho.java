7ce39d445b9d7ccb41b0b8ee547d01d60cc00ab0
CaioCosta23 <caio.c.lopes@edu.ufes.br>
2026-01-11 10:31:29 -0300

    Adicionando Enumeration para caminhos de arquivos

---
+++ src/EnumCaminho.java
@@ -0,0 +1,18 @@
+public enum EnumCaminho {
+    BARBEARIA("infoBarbearia.txt"),
+    AGENDAMENTO("dados/agendamento.txt"),
+    BARBEIROS("dados/barbeiros.txt"),
+    CLIENTES("dados/clientes.txt"),
+    SERVICOS("dados/servicos.txt"),
+    ENDERECO("endereco.txt");
+
+    private final String value;
+    
+    EnumCaminho(String caminho) {
+        this.value = caminho;
+    }
+
+    public String getValue() {
+        return this.value;
+    }
+}
