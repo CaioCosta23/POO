fa99606fb501de651c9dc9f1e71fc5693f2d9a0a
CaioCosta23 <caio.c.lopes@edu.ufes.br>
2026-01-12 12:02:32 -0300

    Adicionando código de recursos

--- src/Recurso.java
+++ src/Recurso.java
@@ -1,3 +1,63 @@
 public class Recurso {
-    
+    private int id;
+    private String nome;
+    private String tipo;
+    private boolean disponivel;
+
+    public Recurso(int id, String nome, String tipo, boolean disponivel) {
+        this.id = id;
+        this.nome = nome;
+        this.tipo = tipo;
+        this.disponivel = disponivel;
+    }
+
+    public Recurso(Recurso original) {
+        this.id = original.id;
+        this.nome = original.nome;
+        this.tipo = original.tipo;
+        this.disponivel = original.disponivel;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTipo() {
+        return this.tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public boolean isDisponivel() {
+        return this.disponivel;
+    }
+
+    public void setDisponivel(boolean disponivel) {
+        this.disponivel = disponivel;
+    }
+
+    public boolean verificarDisponibilidade() {
+        return this.disponivel;
+    }
+
+    public void exibirInformacoes() {
+        System.out.println("- ID: " + this.getId());
+        System.out.println("- Nome: " + this.getNome());
+        System.out.println("- Tipo: " + this.getTipo());
+        System.out.println("- Disponível: " + (this.isDisponivel() ? "Sim" : "Não"));
+    }
 }
