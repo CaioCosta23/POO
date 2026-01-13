a697db80555e31e64ea69e6ae4c8e8541caaa3ad
CaioCosta23 <caio.c.lopes@edu.ufes.br>
2026-01-12 23:58:36 -0300

    Reformulando funções de consultas a usuarios

--- src/Consulta.java
+++ src/Consulta.java
@@ -1,26 +1,46 @@
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 public class Consulta {
     public Consulta() {
     }
 
-    public void exibirClientes(Map<String, Cliente> clientes) {       
-        for (Map.Entry<String, Cliente> valor : clientes.entrySet()) {
-            valor.getValue().exibirInformacoes();
+    public Cliente getCliente(Set<Cliente> clientes, String identificador) {
+        Cliente cliente = null;
+
+        Iterator<Cliente> iterator = clientes.iterator();
+
+        while(iterator.hasNext()) {
+            cliente = iterator.next();
+            if (cliente.getCpf().equals(identificador)) {
+                break;
+            }
+        }
+        return cliente;
+    }
+
+
+    public void exibirClientes(Map<String, Cliente> clientes) {
+            
+        for (Map.Entry<String, Cliente> cliente : clientes.entrySet()) {
+            cliente.getValue().exibirInformacoes();
             System.out.println("----------------------------------------------------------------------------------------------------------------");
         }
     }
 
 
-    public void exibirBarbeiros(Map<String, Barbeiro> barbeiros) {       
-        for (Map.Entry<String, Barbeiro> valor : barbeiros.entrySet()) {
-            valor.getValue().exibirInformacoes();
+
+    public void exibirBarbeiros(Map<String, Barbeiro> barbeiros) {
+            
+        for (Map.Entry<String, Barbeiro> barbeiro : barbeiros.entrySet()) {
+            barbeiro.getValue().exibirInformacoes();
             System.out.println("----------------------------------------------------------------------------------------------------------------");
         }
     }
 
+    
 
     public void exibirServicos(Map<Integer, Servico> servicos) {
         for(Map.Entry<Integer, Servico> valor : servicos.entrySet()) {
