5251e1bcf7602c26ba2294f0b4100af1b3ea7c22
CaioCosta23 <caio.c.lopes@edu.ufes.br>
2026-01-13 23:54:03 -0300

    Retirando atributo de login e editando menu de exibição

--- src/Cliente.java
+++ src/Cliente.java
@@ -3,8 +3,8 @@ import java.time.LocalDate;
 public class Cliente extends Usuario {
     private LocalDate nascimento;
     
-    public Cliente(int id, String nome, String email, String telefone, String cpf, String login, String senha, LocalDate nascimento) {
-        super(id, nome, email, telefone, cpf, login, senha);
+    public Cliente(int id, String nome, String email, String telefone, String cpf, String senha, LocalDate nascimento) {
+        super(id, nome, email, telefone, cpf, senha);
         this.nascimento = nascimento;
     }
 
@@ -23,18 +23,17 @@ public class Cliente extends Usuario {
 
     @Override
     public String toString() {
-        return String.format("%d;%s;%s;%s;%s;%s;%s;%02d/%02d/%04d", this.getId(), this.getNome(), this.getEmail(), this.getTelefone(),
-                                                     this.getCpf(), this.getLogin(), this.getSenha(),
+        return String.format("%d;%s;%s;%s;%s;%s;%02d/%02d/%04d", this.getId(), this.getNome(), this.getEmail(), this.getTelefone(),
+                                                     this.getCpf(), this.getSenha(),
                                                      this.nascimento.getDayOfMonth(), this.nascimento.getMonthValue(),
                                                      this.nascimento.getYear());
     }
 
-    @Override
-    public void exibirMenu() {
-        System.out.println("[1] Cadastrar-se\t[2] Fazer Agendamento");
-        System.out.println("[3]Consultar Servicos\t[4] Cancelar Agendamento");
-        System.out.println("[5] Consultar Dados Pessoais\t [6] Alterar Senha de Usuario");
-        System.out.println("[7] Excluir Usuario");
+    public static void exibirMenu() {
+        System.out.println("[1] Fazer agendamento\t\t\t[2] Cancelar Agendamento");
+        System.out.println("[3] Consultar dados pessoais\t\t[4] Consultar agendamento");
+        System.out.println("[5] Visualizar lista de servicos\t[6] Visualizar lista de prestadores de servicos");
+        System.out.println("[7] Alterar senha de usuario\t\t[8] Excluir Usuario");
     }
 
 
