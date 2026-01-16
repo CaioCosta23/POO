ce3f235c9ffb107a4d5bc1f585ae7325a20797c7
CaioCosta23 <caio.c.lopes@edu.ufes.br>
2026-01-16 18:26:08 -0300

    Editando impressão de notificação

--- src/EnumTipoNotificacao.java
+++ src/EnumTipoNotificacao.java
@@ -1,28 +1,27 @@
-
 public enum EnumTipoNotificacao {
 
     CONFIRMACAO_AGANDAMENTO {
         @Override
         public void notificacao() {
-            System.out.println("Agendamento CONFIRMADO!");
+            System.out.println("+ Agendamento CONFIRMADO!");
         }
     },
     CANCELAMENTO {
         @Override
         public void notificacao() {
-            System.out.println("Agendamento CANCELADO!");
+            System.out.println("+ Agendamento CANCELADO!");
         }
     },
     LEMBRETE_24H {
         @Override
         public void notificacao() {
-            System.out.println("⏰ Lembrete: Seu agendamento é em 24 horas!");
+            System.out.println("+ Lembrete: Seu agendamento é em 24 horas!");
         }
     },
     NOVOS_SERVICOS {
         @Override
         public void notificacao() {
-            System.out.println("✨ Novos serviços disponíveis! Confira agora!");
+            System.out.println("+ Novos serviços disponíveis! Confira agora!");
         }
     };
 
